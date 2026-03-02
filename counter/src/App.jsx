2a0cdad035eb8996ab9673724a172c0c76ff55cd
Talha Ahmed <talhaahmed5126@gmail.com>
2026-03-02 06:36:34 +0500

    Counter done

---
+++ counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(10)
+
+  const addValue = () => {
+    setCounter(prevCounter => {
+      if (prevCounter < 20) {
+        return prevCounter + 1
+      }
+      return prevCounter
+    })
+  }
+
+  const removeValue = () => {
+    setCounter(prevCounter => {
+      if (prevCounter > 0) {
+        return prevCounter - 1
+      }
+      return prevCounter
+    })
+  }
+
+  return (
+    <>
+      <h1>Counter by React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>
+        Add value
+      </button>
+
+      <br /><br />
+
+      <button onClick={removeValue}>
+        Remove value
+      </button>
+
+      <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
